16f18930fdd3199b5c6c125b23811af45f87e1f5
TeNg <tenm@urbox.vn>
2023-07-24 15:47:00 +0700

    fix: race condition

--- race.go
+++ race.go
@@ -3,6 +3,7 @@ package executor
 import (
 	"context"
 	"errors"
+	"sync"
 	"time"
 )
 
@@ -14,7 +15,7 @@ type RaceFn = func(...interface{}) (interface{}, error)
 
 // RaceFn any action done func return
 func Race(duration time.Duration, fn RaceFn, params ...interface{}) (interface{}, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	ctx, cancel := context.WithCancel(context.Background())
 	var result interface{}
 	var err error
 	go func() {
@@ -32,9 +33,73 @@ func Race(duration time.Duration, fn RaceFn, params ...interface{}) (interface{}
 	}
 }
 
+// Race2Fn any action done func return
+func Race2Task(fn1, fn2 RaceFn, duration time.Duration, params ...interface{}) (interface{}, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	var result interface{}
+	var err error
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		wg.Done()
+		_result, _err := fn1(params...)
+		result = _result
+		err = _err
+		cancel()
+	}()
+	go func() {
+		wg.Done()
+		_result, _err := fn2(params...)
+		result = _result
+		err = _err
+		cancel()
+	}()
+	wg.Wait()
+
+	select {
+	case <-ctx.Done():
+		return result, err
+	case <-time.After(duration):
+		return nil, errors.New(E_TimeUp)
+	}
+}
+
+// RaceFn any action done func return
+func Race2TaskWithSafeQueue(sq ISafeQueue, fn1, fn2 RaceFn, duration time.Duration, params ...interface{}) (interface{}, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	var result interface{}
+	var err error
+
+	sq.Send(&Job{
+		Params: params,
+		Exectutor: func(i ...interface{}) (interface{}, error) {
+			_result, _err := fn1(params...)
+			result = _result
+			err = _err
+			cancel()
+			return _result, _err
+		},
+	}, &Job{
+		Params: params,
+		Exectutor: func(i ...interface{}) (interface{}, error) {
+			_result, _err := fn2(params...)
+			result = _result
+			err = _err
+			cancel()
+			return _result, _err
+		},
+	})
+	select {
+	case <-ctx.Done():
+		return result, err
+	case <-time.After(duration):
+		return nil, errors.New(E_TimeUp)
+	}
+}
+
 // RaceFn any action done func return
 func RaceWithSafeQueue(sq ISafeQueue, duration time.Duration, fn RaceFn, params ...interface{}) (interface{}, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	ctx, cancel := context.WithCancel(context.Background())
 	var result interface{}
 	var err error
 
--- race_test.go
+++ race_test.go
@@ -60,3 +60,41 @@ func TestRaceWithExecutor(t *testing.T) {
 		t.Fail()
 	}
 }
+
+// func fnTest(i ...interface{}) (interface{}, error) {
+// 	duration := i[0].(time.Duration)
+// 	time.Sleep(duration * time.Millisecond)
+// 	return nil, nil
+// }
+
+func fn100(i ...interface{}) (interface{}, error) {
+	time.Sleep(100 * time.Millisecond)
+	return "100", nil
+}
+func fn300(i ...interface{}) (interface{}, error) {
+	time.Sleep(300 * time.Millisecond)
+	return "300", nil
+}
+func TestRace2FnSimple(t *testing.T) {
+	now := time.Now()
+	out, err := Race2Task(fn300, fn100, 500*time.Millisecond)
+	log.Print("run: ", time.Since(now))
+	if err != nil {
+		log.Print(err)
+	}
+	if out.(string) != "100" {
+		log.Print(out)
+	}
+
+	now = time.Now()
+	out, err = Race2Task(fn300, fn100, 50*time.Millisecond)
+	log.Print("run: ", time.Since(now))
+	if err != nil {
+		log.Print(err)
+	}
+	log.Print(out, err)
+	// if out.(string) != "100" {
+	// 	log.Print(out)
+	// }
+
+}
